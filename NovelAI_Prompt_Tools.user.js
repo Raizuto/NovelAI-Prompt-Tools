c2319d8007b771dbce0683c66545afce1c8a4b2a
Raizuto <42843413+Raizuto@users.noreply.github.com>
2026-02-15 10:46:04 -0600

    Update NovelAI_Prompt_Tools.user.js

--- NovelAI_Prompt_Tools.user.js
+++ NovelAI_Prompt_Tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NovelAI Prompt Tools
 // @namespace    https://github.com/Raizuto/NovelAI-Prompt-Tools/tree/main-forked
-// @version      4.9.7
+// @version      4.9.8
 // @description  A simple Tampermonkey userscript for NovelAI Image Generator that makes prompting easier with a real-time tag suggestion and fast tag weight functionality.
 // @author       x1101 & Raizuto
 // @match        https://novelai.net/image
